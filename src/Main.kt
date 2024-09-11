a2c1b943d10afd8051ef9d6e65b8c714673f9a7b
Lester Cheong <lestercheong70+github@outlook.com>
2024-09-11 22:25:42 +0800

    modify: added args[0] as name

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,14 @@
-fun main() {
-    val name = "Kotlin"
+import kotlin.system.exitProcess
+
+fun main(args: Array<String>) {
+    val name: String = try {
+        args[0]
+    } catch (e: Exception) {
+        println(e)
+        println("did not find args[0] - exiting program with exitcode 1")
+        exitProcess(1)
+
+    }
     println("Hello, " + name + "!")
 
     for (i in 1..5) {
