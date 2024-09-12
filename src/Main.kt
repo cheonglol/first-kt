f84d3ac3599f916cb1f123403bd123b6f1f087ab
Lester Cheong <lestercheong70+github@outlook.com>
2024-09-12 23:19:38 +0800

    code: played with MutableList<T>

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,4 @@
+import kotlin.random.Random
 import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
@@ -11,7 +12,45 @@ fun main(args: Array<String>) {
     }
     println("Hello, " + name + "!")
 
-    for (i in 1..5) {
-        println("i = $i")
+    for (i: Int in 1..5) {
+        val randomInt: Double = Random.nextDouble(from = 1.0, until = 100.0)
+        val product: Double = i * randomInt
+        println("i = $i | (Random Value)$randomInt * $i, \ngives product of $product\n")
     }
+    println("\n")
+
+    print("Creating a mutableList<String>... Add the first item\n")
+    val input: String = readln()
+    if (input.equals("")) println("no input received, skipping this part of the code")
+    else {
+        appendMutableListOfString(input)
+    }
+}
+
+fun appendMutableListOfString(initialValue: String): MutableList<String> {
+    val stringStore: MutableList<String> = mutableListOf(initialValue)
+    printLineOfMutableListOfString(stringStore)
+    while (true) {
+        val input: String = getStringFromReadline(null)
+        if (input.equals("")) {
+            break
+        }
+        stringStore.add(input)
+        printLineOfMutableListOfString(stringStore)
+    }
+    printLineOfMutableListOfString(stringStore)
+    return stringStore
+}
+
+fun getStringFromReadline(lineMessage: String?): String {
+    println("(Enter with an empty line to stop adding)")
+    if (lineMessage != null) println(lineMessage + "\n")
+    val input = readLine() ?: ""
+    return input
+}
+
+fun printLineOfMutableListOfString(stringStore: MutableList<String>): Unit {
+    println("[")
+    for (s: String in stringStore) println("\t\"$s\",")
+    println("]")
 }
